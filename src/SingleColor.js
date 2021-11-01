37c2d9010e2009014a4903d23b803cd9f25924a4
Zafar Ansari <zafar.ansari.892@gmail.com>
2021-11-01 21:52:40 +0530

    Initial commit

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,19 +1,9 @@
 import React, { useState, useEffect } from "react";
-import rgbToHex from "./utils";
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
   let bcg = rgb.join(",");
-  //   ist way -
-  // this is the function which is convert rgb value to hex value and in this function we pass our all rgb color with spread operator
 
-  //  2nd way -
-  //  pass the hex value as a prop in the color.hex and than u this in where u want this way when u dont want to use any ohter function like rgbToHex. this the simplest way.
-  //   but in this if u want # symbol y have to set manually in the ist ways u dont have to set manually it is the   minor diiff in two ways
-
-  // const hex = rgbToHex(...rgb);
-
-  // set # symbol manually -
   const hexValue = `#${hexColor}`;
   console.log(bcg);
 
